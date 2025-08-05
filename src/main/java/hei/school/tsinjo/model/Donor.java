eb3ea93ef428351f15fa6a3ebae470dc0fe22923
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-05 16:35:40 +0300

    fixup

--- src/main/java/hei/school/tsinjo/model/Donor.java
+++ src/main/java/hei/school/tsinjo/model/Donor.java
@@ -1,8 +1,7 @@
 package hei.school.tsinjo.model;
 
-import lombok.Builder;
 
-@Builder
+@Getter
 public class Donor extends Person {
   public Donor(String email, String fullName) {
     super(email, fullName);
